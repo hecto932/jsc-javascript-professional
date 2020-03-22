1748156c40cbd3874be576c886242054ea8d4fc9
Hector Flores <Hector_Flores@eogresources.com>
2020-03-21 20:10:19 -0700

    Using visibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
 
   run(player) {
@@ -12,7 +13,20 @@ class AutoPause {
     })
 
     observer.observe(this.player.media)
+
+    document.addEventListener('visibilitychange', this.handleVisibilityChange)
   }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible'
+
+    if (isVisible) {
+      this.player.play()
+    } else {
+      this.player.pause()
+    }
+  }
+
 
   handleIntersection(entries) {
     const entry = entries[0]
