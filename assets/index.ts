5cdc719e057a27a0b55845444403715553b41af9
Hector Flores <Hector_Flores@eogresources.com>
2020-03-22 20:05:10 -0700

    Changing the projecto to TypeScript

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,6 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
-import AutoPause from './plugins/AutoPause.ts';
+import MediaPlayer from './MediaPlayer';
+import AutoPlay from './plugins/AutoPlay';
+import AutoPause from './plugins/AutoPause';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
@@ -11,10 +11,10 @@ const player = new MediaPlayer({
 const button = document.querySelector('button');
 button.onclick = () => player.togglePlay();
 
-const playButton = document.querySelector('#playButton');
+const playButton: HTMLElement = document.querySelector('#playButton');
 playButton.onclick = () => player.togglePlay();
 
-const muteButton = document.querySelector('#muteButton');
+const muteButton: HTMLElement = document.querySelector('#muteButton');
 muteButton.onclick = () => {
   if (player.media.muted) {
     player.unmute();
