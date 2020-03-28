db97114053968083362883fffe1f666c9ee249f9
Hector Flores <Hector_Flores@eogresources.com>
2020-03-28 14:06:34 -0700

    Singleton pattern

---
+++ ejercicios/singleton/singleton.ts
@@ -0,0 +1,17 @@
+class Singleton {
+  private static instance: Singleton;
+
+  private constructor() {
+    // INIT
+  }
+
+  static getInstance() {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton();
+    }
+
+    return Singleton.instance;
+  }
+}
+
+export default Singleton;
