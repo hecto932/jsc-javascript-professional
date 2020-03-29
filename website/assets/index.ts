86ade615d1f6eb89f554a709d48e999de3c696d5
Hector Flores <Hector_Flores@eogresources.com>
2020-03-29 14:38:55 -0700

    Package update to shows up ads

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from './MediaPlayer';
-import AutoPlay from './plugins/AutoPlay';
-import AutoPause from './plugins/AutoPause';
-import Ads from './plugins/Ads';
+import MediaPlayer from '@hecto932/platzi-mediaplayer';
+import AutoPlay from '@hecto932/platzi-mediaplayer/lib/plugins/AutoPlay';
+import AutoPause from '@hecto932/platzi-mediaplayer/lib/plugins/AutoPause';
+import Ads from '@hecto932/platzi-mediaplayer/lib/plugins/Ads';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
